821620a922885fbd91278250ad76e10f7eb31897
edwardmcn64 <edwardmcn64@gmail.com>
2013-10-22 23:26:52 -0400

    Added layout and activity for viewing user statistics

--- gen/com/egyptianratscrew/R.java
+++ gen/com/egyptianratscrew/R.java
@@ -306,21 +306,23 @@ public final class R {
     public static final class id {
         public static final int LinearLayout1=0x7f090005;
         public static final int Table=0x7f090006;
-        public static final int action_settings=0x7f090014;
+        public static final int action_settings=0x7f090016;
         public static final int btnComputer=0x7f090002;
         public static final int btnHuman=0x7f090003;
         public static final int btnRules=0x7f090001;
         public static final int btnStatistics=0x7f090004;
         public static final int txtAppName=0x7f090000;
         public static final int txtDisplayEmail=0x7f09000a;
+        public static final int txtDisplayHighScore=0x7f090015;
         public static final int txtDisplayName=0x7f09000b;
-        public static final int txtDisplayNumberOfLosses=0x7f090011;
-        public static final int txtDisplayNumberOfTies=0x7f090012;
-        public static final int txtDisplayNumberOfWins=0x7f090010;
-        public static final int txtDisplayTotalGames=0x7f090013;
+        public static final int txtDisplayNumberOfLosses=0x7f090012;
+        public static final int txtDisplayNumberOfTies=0x7f090013;
+        public static final int txtDisplayNumberOfWins=0x7f090011;
+        public static final int txtDisplayTotalGames=0x7f090014;
         public static final int txtDisplayUserName=0x7f090009;
         public static final int txtRules=0x7f090007;
         public static final int txtRulesDescription=0x7f090008;
+        public static final int txtStatHighScore=0x7f090010;
         public static final int txtStatLoss=0x7f09000d;
         public static final int txtStatTies=0x7f09000e;
         public static final int txtStatTotal=0x7f09000f;
@@ -342,6 +344,7 @@ public final class R {
          */
         public static final int computer=0x7f060004;
         public static final int hello_world=0x7f060002;
+        public static final int highScore=0x7f06000c;
         public static final int human=0x7f060005;
         public static final int numberOfLosses=0x7f060009;
         public static final int numberOfTies=0x7f06000a;
