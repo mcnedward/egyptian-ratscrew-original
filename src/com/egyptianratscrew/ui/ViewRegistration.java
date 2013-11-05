12b6ab9e4ffe3c87975961c4fdc683293a016135
jufrazee <jufrazee@gmail.com>
2013-11-04 20:34:36 -0500

    registration class

--- src/com/egyptianratscrew/ui/ViewRegistration.java
+++ src/com/egyptianratscrew/ui/ViewRegistration.java
@@ -1,7 +1,7 @@
 package com.egyptianratscrew.ui;
 
+
 import com.egyptianratscrew.R;
-import com.egyptianratscrew.dao.User;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -14,15 +14,8 @@ public class ViewRegistration extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register);
-		
-		User player = new User();
-		displayPlayerData(player);
-		
-  }
-	private void displayPlayerData(User user) {
-		
-		
 	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
