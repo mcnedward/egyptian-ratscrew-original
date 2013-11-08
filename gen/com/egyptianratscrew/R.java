2cf2c1a9f69b1eaf1a8e2482c908d4e101c3ecf7
edwardmcn64 <edwardmcn64@gmail.com>
2013-11-07 20:33:08 -0500

    Fixed errors in activities with importing R files by removing bad code from layout xmls

--- gen/com/egyptianratscrew/R.java
+++ gen/com/egyptianratscrew/R.java
@@ -304,39 +304,47 @@ public final class R {
         public static final int sq=0x7f020044;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f090007;
-        public static final int Table=0x7f090008;
-        public static final int action_settings=0x7f090018;
+        public static final int LinearLayout1=0x7f09000d;
+        public static final int Table=0x7f09000e;
+        public static final int TextView01=0x7f09000a;
+        public static final int TextView02=0x7f090009;
+        public static final int action_settings=0x7f09001f;
         public static final int btnComputer=0x7f090002;
+        public static final int btnDeveloper=0x7f090007;
         public static final int btnFBLogin=0x7f090006;
         public static final int btnHuman=0x7f090003;
         public static final int btnRules=0x7f090001;
         public static final int btnStatistics=0x7f090004;
         public static final int btnUserRegister=0x7f090005;
+        public static final int textView1=0x7f09000b;
+        public static final int textView2=0x7f09000c;
         public static final int txtAppName=0x7f090000;
-        public static final int txtDisplayEmail=0x7f09000c;
-        public static final int txtDisplayHighScore=0x7f090017;
-        public static final int txtDisplayName=0x7f09000d;
-        public static final int txtDisplayNumberOfLosses=0x7f090014;
-        public static final int txtDisplayNumberOfTies=0x7f090015;
-        public static final int txtDisplayNumberOfWins=0x7f090013;
-        public static final int txtDisplayTotalGames=0x7f090016;
-        public static final int txtDisplayUserName=0x7f09000b;
-        public static final int txtRules=0x7f090009;
-        public static final int txtRulesDescription=0x7f09000a;
-        public static final int txtStatHighScore=0x7f090012;
-        public static final int txtStatLoss=0x7f09000f;
-        public static final int txtStatTies=0x7f090010;
-        public static final int txtStatTotal=0x7f090011;
-        public static final int txtStatWin=0x7f09000e;
+        public static final int txtDeveloper=0x7f090008;
+        public static final int txtDisplayEmail=0x7f090013;
+        public static final int txtDisplayHighScore=0x7f09001e;
+        public static final int txtDisplayName=0x7f090014;
+        public static final int txtDisplayNumberOfLosses=0x7f09001b;
+        public static final int txtDisplayNumberOfTies=0x7f09001c;
+        public static final int txtDisplayNumberOfWins=0x7f09001a;
+        public static final int txtDisplayTotalGames=0x7f09001d;
+        public static final int txtDisplayUserName=0x7f090012;
+        public static final int txtNewUser=0x7f09000f;
+        public static final int txtRules=0x7f090010;
+        public static final int txtRulesDescription=0x7f090011;
+        public static final int txtStatHighScore=0x7f090019;
+        public static final int txtStatLoss=0x7f090016;
+        public static final int txtStatTies=0x7f090017;
+        public static final int txtStatTotal=0x7f090018;
+        public static final int txtStatWin=0x7f090015;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fblogin=0x7f030001;
-        public static final int game_layout=0x7f030002;
-        public static final int register=0x7f030003;
-        public static final int rules=0x7f030004;
-        public static final int statistics=0x7f030005;
+        public static final int developer=0x7f030001;
+        public static final int fblogin=0x7f030002;
+        public static final int game_layout=0x7f030003;
+        public static final int register=0x7f030004;
+        public static final int rules=0x7f030005;
+        public static final int statistics=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f080000;
