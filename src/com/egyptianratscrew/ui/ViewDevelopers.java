ec58fc70b555487dfdfb100d41f61dcfbed4024c
jufrazee <jufrazee@localhost>
2013-11-09 13:55:22 -0500

    finishing the developer class

---
+++ src/com/egyptianratscrew/ui/ViewDevelopers.java
@@ -0,0 +1,31 @@
+package com.egyptianratscrew.ui;
+
+
+import com.egyptianratscrew.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+/**
+ 
+ * 
+ */
+
+public class ViewDevelopers extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.manager);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
+
