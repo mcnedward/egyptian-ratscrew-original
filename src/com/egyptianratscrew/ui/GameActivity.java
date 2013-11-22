c2bc45c2439c51837e3bdcdfdf77c034bfd75604
edwardmcn64 <edwardmcn64@gmail.com>
2013-11-22 03:29:45 -0500

    Cleaned up Game class and added Iterator for removing cards from computer's hand and adding those cards to the middle stack. Also added a new Timer to check whether the computer should play a card. Turn taking still not working perfectly, but much better than before and no longer throwing errors from ConcurrentModificationException or IndexOutOfBounds. Still need to implement Game logic to new playCard method to work properly on SurfaceView drawing. Also cleaned up GameSurface class and commented out code that is unnecessary for now. Added java doc comments to GameSurface and some to Game Class.
    Updated Card class with new onDraw method, rotation method, and height and width parameters. Changed Game class to set the card coordinates and the listener for touching the player's hand. Changed the GameSurface display card methods to first change the coordinates in the game class, then draw each card in each player's hand as well as the cards in the middle stack. These changes fix the crashes caused by ConcurrentModificationExceptions by keeping setting card coordinates, checking whether or not to remove cards, and canvas drawing all in separate loops, and iterating through each one correctly. Also added better java doc comments to the Card class.

--- src/com/egyptianratscrew/ui/GameActivity.java
+++ src/com/egyptianratscrew/ui/GameActivity.java
@@ -1,6 +1,5 @@
 package com.egyptianratscrew.ui;
 
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
@@ -11,26 +10,37 @@ import com.egyptianratscrew.service.GameSurface;
 
 public class GameActivity extends Activity {
 
-	//declaring variables
+	// declaring variables
 	private RelativeLayout table;
 	private GameSurface tableCardSurface;
 	private Game game;
-	
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState){
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.game_layout);
-		
-		//game = new Game(true, this.getIntent().getIntExtra(Difficulty,3), new String[] {this.getIntent().getStringExtra(Player1Name),this);
-		game = new Game(true, 3, new String[] {"Player1"}, this);
-		
-		//setting the relativelayout of table
+
+		// game = new Game(true, this.getIntent().getIntExtra(Difficulty,3), new String[]
+		// {this.getIntent().getStringExtra(Player1Name),this);
+		game = new Game(true, 3, new String[] { "Player1" }, this);
+
+		// setting the relative layout of table
 		table = (RelativeLayout) findViewById(R.id.Table);
 		tableCardSurface = new GameSurface(this, game);
-		//adding the tableCardSurface
+		// adding the tableCardSurface
 		table.addView(tableCardSurface);
-		
-		
-		
+
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		tableCardSurface.pause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		tableCardSurface.resume();
 	}
 }
