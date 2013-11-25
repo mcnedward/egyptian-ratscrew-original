1390ff60c8961b859e56f1c55b16335961278ada
StopNowThink <StopNowThink@localhost>
2013-11-25 14:51:48 -0500

    Updated ResultActivity. Made User Serializable.

--- src/com/egyptianratscrew/dao/User.java
+++ src/com/egyptianratscrew/dao/User.java
@@ -1,6 +1,8 @@
 package com.egyptianratscrew.dao;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable  {
 
 	private int userId;
 	private String userName;
