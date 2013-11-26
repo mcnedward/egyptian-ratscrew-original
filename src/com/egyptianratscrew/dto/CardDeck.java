6a29fe1471481d5a72b1e37f4f2ddb46da2f3e97
edwardmcn64 <edwardmcn64@gmail.com>
2013-11-26 16:34:27 -0500

    Changed Game to make playCard method work, and displayCards to be separate from playing cards.

--- src/com/egyptianratscrew/dto/CardDeck.java
+++ src/com/egyptianratscrew/dto/CardDeck.java
@@ -13,7 +13,7 @@ public class CardDeck {
 	private String[] deck = new String[] { "sa", "s2", "s3", "s4", "s7", "s6", "s7", "s8", "s9", "s10", "sj", "sq",
 			"sk", "ca", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "cj", "cq", "ck", "da", "d2", "d3",
 			"d4", "d5", "d6", "d7", "d8", "d9", "d10", "dj", "dq", "dk", "ha", "h2", "h3", "h4", "h5", "h6", "h7",
-			"h8", "h9", "h10", "hj", "hq", "hk" };
+			"h8", "h9", "h10", "hj", "h10", "h10" };
 
 	public CardDeck() {
 
