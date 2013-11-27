1c2c2a9b286fb18007127db5a57377096b9fe4fe
StopNowThink <StopNowThink@localhost>
2013-11-27 09:26:21 -0500

    Added UserArrayWrapper class

--- src/com/egyptianratscrew/dao/IGameFinishedListener.java
+++ src/com/egyptianratscrew/dao/IGameFinishedListener.java
@@ -1,7 +1,9 @@
 package com.egyptianratscrew.dao;
 
+import com.egyptianratscrew.service.Game;
+
 public interface IGameFinishedListener {
 
-    public void onGameFinished(GameView gameView);
+    public void onGameFinished(Game game);
 
 }
---
+++ src/com/egyptianratscrew/dto/ComputerPlayer.java
@@ -0,0 +1,12 @@
+package com.egyptianratscrew.dto;
+
+import com.egyptianratscrew.dao.User;
+
+public class ComputerPlayer extends HumanPlayer {
+
+	public ComputerPlayer(User u, int id) {
+		super(u, id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
