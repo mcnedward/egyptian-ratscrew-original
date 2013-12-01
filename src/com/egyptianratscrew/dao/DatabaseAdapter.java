4d0220b29f511ff5f3ed745444841fda1663ca4c
edwardmcn64 <edwardmcn64@gmail.com>
2013-12-01 16:20:11 -0500

    Changed the layout of the Registration page. Changed the naming conventions for the database. Added open and close methods to the RatscrewDatabase. This allows for insertion of users to work correctly now. Also added a userNameExists method, and fixed the userExists method to work properly. Added javadoc comments to new fixes. Changed layout of parameters in User class. Added new constructor to be used when creating a new user by registration. Overrode all methods in User class so that the implementation of IUser will work. Fixed RegisterActivity to check for empty strings, check if user exists, and check if passwords match. Adding new users now works.

--- src/com/egyptianratscrew/dao/DatabaseAdapter.java
+++ src/com/egyptianratscrew/dao/DatabaseAdapter.java
@@ -18,23 +18,23 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
 	/** Database Name **/
 	private static String DB_NAME = "ratscrew.db";
 	/** Database Version **/
-	private static final int DB_VERSION = 1;
+	private static final int DB_VERSION = 2;
 
 	/** User Table **/
 	private static String DATABASE_TABLE_USER = "user";
 	private static String USER_ID = "_id";
-	private static String USERNAME = "userName";
-	private static String PASSWORD = "password";
-	private static String EMAIL = "email";
-	private static String FIRST_NAME = "first_name";
-	private static String LAST_NAME = "last_name";
-	private static String NUMBER_OF_WINS = "number_of_wins";
-	private static String NUMBER_OF_LOSSES = "number_of_losses";
-	private static String HIGHEST_WINNING_STREAK = "highest_winning_streak";
-	private static String HIGHEST_LOSING_STREAK = "highest_losing_streak";
-	private static String NUMBER_OF_TIES = "number_of_ties";
-	private static String TOTAL_GAMES = "total_games";
-	private static String HIGH_SCORE = "high_score";
+	private static String FIRST_NAME = "FirstName";
+	private static String LAST_NAME = "LastName";
+	private static String USERNAME = "UserName";
+	private static String EMAIL = "Email";
+	private static String PASSWORD = "Password";
+	private static String NUMBER_OF_WINS = "NumberOfWins";
+	private static String NUMBER_OF_LOSSES = "NumberOfLosses";
+	private static String HIGHEST_WINNING_STREAK = "HighestWinningStreak";
+	private static String HIGHEST_LOSING_STREAK = "HighestLosingStreak";
+	private static String NUMBER_OF_TIES = "NumberOfTies";
+	private static String TOTAL_GAMES = "TotalGames";
+	private static String HIGH_SCORE = "HighScore";
 
 	/**
 	 * Constructor Takes and keeps a reference of the passed context in order to
--- src/com/egyptianratscrew/dao/RatscrewDatabase.java
+++ src/com/egyptianratscrew/dao/RatscrewDatabase.java
@@ -9,54 +9,90 @@ import android.database.DatabaseUtils.InsertHelper;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
-
 public class RatscrewDatabase {
 	public static final int USER_NAME_FIELD = 2;
-
 	public static final int FIRST_NAME_FIELD = 1;
-
-	private static String TAG = "MusicDatabase";
+	private static String TAG = "RatscrewDatabase";
 
 	private DatabaseAdapter dba;
 	private SQLiteDatabase db;
 
-	/** User Variables **/
+	/** User Table **/
 	private static String DATABASE_TABLE_USER = "user";
 	private static String USER_ID = "_id";
-	private static String USERNAME = "userName";
-	private static String PASSWORD = "password";
-	private static String EMAIL = "email";
-	private static String FIRST_NAME = "first_name";
-	private static String LAST_NAME = "last_name";
-	private static String NUMBER_OF_WINS = "number_of_wins";
-	private static String NUMBER_OF_LOSSES = "number_of_losses";
-	private static String HIGHEST_WINNING_STREAK = "highest_winning_streak";
-	private static String HIGHEST_LOSING_STREAK = "highest_losing_streak";
-	private static String NUMBER_OF_TIES = "number_of_ties";
-	private static String TOTAL_GAMES = "total_games";
-	private static String HIGH_SCORE = "high_score";
+	private static String FIRST_NAME = "FirstName";
+	private static String LAST_NAME = "LastName";
+	private static String USERNAME = "UserName";
+	private static String EMAIL = "Email";
+	private static String PASSWORD = "Password";
+	private static String NUMBER_OF_WINS = "NumberOfWins";
+	private static String NUMBER_OF_LOSSES = "NumberOfLosses";
+	private static String HIGHEST_WINNING_STREAK = "HighestWinningStreak";
+	private static String HIGHEST_LOSING_STREAK = "HighestLosingStreak";
+	private static String NUMBER_OF_TIES = "NumberOfTies";
+	private static String TOTAL_GAMES = "TotalGames";
+	private static String HIGH_SCORE = "HighScore";
+
+	public RatscrewDatabase() {
+
+	}
 
 	public RatscrewDatabase(Context context) {
 		dba = new DatabaseAdapter(context);
+	}
+
+	/**
+	 * Open the database for the Music Library for reading only.
+	 * 
+	 * @return A read-only database.
+	 * @throws android.database.SQLException
+	 */
+	public RatscrewDatabase openToRead() throws android.database.SQLException {
+		db = dba.getReadableDatabase();
+		return this;
+	}
+
+	/**
+	 * Opens the database for the Music Library for writing.
+	 * 
+	 * @return A writable database.
+	 * @throws android.database.SQLException
+	 */
+	public RatscrewDatabase open() throws android.database.SQLException {
 		db = dba.getWritableDatabase();
+		return this;
+	}
+
+	/**
+	 * Close the open database object.
+	 */
+	public void close() {
+		dba.close();
 	}
 
 	/********** INSERT QUERIES **********/
 
+	/**
+	 * This is used to add a new user to the database.
+	 * 
+	 * @param user
+	 *            The user to add into the database.
+	 */
 	@SuppressWarnings("deprecation")
 	public void insertUser(IUser user) {
 		if (userExists(user.getUserId())) {
 			return;
 		} else {
+			open();
 			// Create a single InsertHelper to handle this set of insertions.
 			InsertHelper ih = new InsertHelper(db, DATABASE_TABLE_USER);
 
 			// Get the Artist information
-			final String userName = user.getUserName();
-			final String password = user.getPassword();
-			final String email = user.getEmail();
 			final String firstName = user.getFirstName();
 			final String lastName = user.getLastName();
+			final String userName = user.getUserName();
+			final String email = user.getEmail();
+			final String password = user.getPassword();
 			final int numberOfWins = user.getNumberOfWins();
 			final int numberOfLosses = user.getNumberOfLosses();
 			final int highestWinningStreak = user.getHighestWinningStreak();
@@ -66,11 +102,11 @@ public class RatscrewDatabase {
 			final int highScore = user.getHighScore();
 
 			// Get the numeric indexes for each of the columns to update
-			final int userNameColumn = ih.getColumnIndex(USERNAME);
-			final int passwordColumn = ih.getColumnIndex(PASSWORD);
-			final int emailColumn = ih.getColumnIndex(EMAIL);
 			final int firstNameColumn = ih.getColumnIndex(FIRST_NAME);
 			final int lastNameColumn = ih.getColumnIndex(LAST_NAME);
+			final int userNameColumn = ih.getColumnIndex(USERNAME);
+			final int emailColumn = ih.getColumnIndex(EMAIL);
+			final int passwordColumn = ih.getColumnIndex(PASSWORD);
 			final int numberOfWinsColumn = ih.getColumnIndex(NUMBER_OF_WINS);
 			final int numberOfLossesColumn = ih.getColumnIndex(NUMBER_OF_LOSSES);
 			final int highestWinningStreakColumn = ih.getColumnIndex(HIGHEST_WINNING_STREAK);
@@ -103,15 +139,25 @@ public class RatscrewDatabase {
 				// Insert the row into the database.
 				ih.execute();
 				x++;
+			} catch (Exception e) {
+				Log.i(TAG, e.getMessage(), e);
 			} finally {
 				ih.close();
+				close();
 			}
 		}
 	}
 
 	/********** SELECT QUERIES **********/
 
-	public User selectUserById(int userId) {
+	/**
+	 * This is used to select a user from the database, based on a user id.
+	 * 
+	 * @param userId
+	 *            The user id of the user you want to return from the database.
+	 * @return The user from the database.
+	 */
+	public IUser selectUserById(int userId) {
 		User user = null;
 		Cursor c = db.rawQuery("SELECT * FROM user WHERE userId = ?", new String[] { String.valueOf(userId) });
 		try {
@@ -143,7 +189,7 @@ public class RatscrewDatabase {
 			}
 		}
 	}
-	
+
 	public IUser selectUserByName(String userFirstName) {
 		IUser user = null;
 		Cursor c = db.rawQuery("SELECT * FROM user WHERE FIRST_NAME = ?", new String[] { userFirstName });
@@ -177,7 +223,7 @@ public class RatscrewDatabase {
 			}
 		}
 	}
-	
+
 	public IUser selectUserByUserName(String aUserName) {
 		IUser user = null;
 		Cursor c = db.rawQuery("SELECT * FROM user WHERE USER_NAME = ?", new String[] { aUserName });
@@ -214,30 +260,73 @@ public class RatscrewDatabase {
 
 	/********** OBJECT EXISTS QUERIES **********/
 
+	/**
+	 * Use this to check whether a user is already in the database.
+	 * 
+	 * @param userId
+	 *            The user id of the user to check against.
+	 * @return True if the user is already in the database, false if that user is not in the database.
+	 */
 	public boolean userExists(int userId) {
-		Cursor c = db.rawQuery("SELECT * FROM user WHERE USER_ID = ?", new String[] { String.valueOf(userId) });
-		if (c.getCount() > 0) {
-			c.close();
-			return true;
-		} else {
-			c.close();
-			return false;
+		open();
+		Cursor c = null;
+		try {
+			c = db.rawQuery("SELECT * FROM " + DATABASE_TABLE_USER + " WHERE " + USER_ID + " = ?",
+					new String[] { String.valueOf(userId) });
+			c.moveToFirst();
+			if (c.getCount() > 0) {
+				return true;
+			}
+		} catch (Exception e) {
+			Log.i(TAG, e.getMessage(), e);
+		} finally {
+			if (c != null && !c.isClosed()) {
+				c.close();
+			}
+			close();
 		}
+		return false;
 	}
-	
+
+	/**
+	 * Use this to check whether a user name is already in use by some one else.
+	 * 
+	 * @param userName
+	 *            The user name that you want to check against.
+	 * @return True if the user name already exists, false if the user name is not in use.
+	 */
+	public boolean userNameExists(String userName) {
+		open();
+		Cursor c = null;
+		try {
+			c = db.rawQuery("SELECT * FROM " + DATABASE_TABLE_USER + " WHERE " + USERNAME + " = ?",
+					new String[] { String.valueOf(userName) });
+			c.moveToFirst();
+			if (c.getCount() > 0) {
+				return true;
+			}
+		} catch (Exception e) {
+			Log.i(TAG, e.getMessage(), e);
+		} finally {
+			if (c != null && !c.isClosed()) {
+				c.close();
+			}
+			close();
+		}
+		return false;
+	}
+
 	public boolean userExists(String userName, int field) {
 		Cursor c = null;
-		if (field == FIRST_NAME_FIELD){
+		if (field == FIRST_NAME_FIELD) {
 			c = db.rawQuery("SELECT * FROM user WHERE FIRST_NAME = ?", new String[] { userName });
-		}
-		else if (field == USER_NAME_FIELD){
+		} else if (field == USER_NAME_FIELD) {
 			c = db.rawQuery("SELECT * FROM user WHERE USER_NAME = ?", new String[] { userName });
-		}
-		else {
+		} else {
 			Log.d("TAG: ", "Field Invalid.");
 			return false;
 		}
-		
+
 		if (c.getCount() > 0) {
 			c.close();
 			return true;
@@ -246,35 +335,28 @@ public class RatscrewDatabase {
 			return false;
 		}
 	}
-	
-	public int getNewUserID()
-	{
+
+	public int getNewUserID() {
 		int retInt = 1;
 		Cursor c = db.rawQuery("SELECT * FROM user", null);
 		try {
 			List<Integer> userIDs = new ArrayList<Integer>();
-			
+
 			while (c.moveToNext()) {
 				userIDs.add(c.getInt(c.getColumnIndexOrThrow(USER_ID)));
 			}
-			
-			while (true)
-			{
-				if (userIDs.contains(retInt))
-				{
+
+			while (true) {
+				if (userIDs.contains(retInt)) {
 					retInt++;
-				}
-				else
-				{
+				} else {
 					break;
 				}
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			Log.d("TAG: ", e.toString());
 		}
-		
+
 		return retInt;
 	}
 
