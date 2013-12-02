e9dae19d04a5bdde220c37fbb2ac6dc125021087
StopNowThink <StopNowThink@localhost>
2013-12-01 19:13:24 -0500

    Finished removing ties and highscore from database

--- src/com/egyptianratscrew/dao/DatabaseAdapter.java
+++ src/com/egyptianratscrew/dao/DatabaseAdapter.java
@@ -34,9 +34,7 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
 	private static String HIGHEST_LOSING_STREAK = "highest_losing_streak";
 	private static String CURRENT_WINNING_STREAK = "current_winning_streak";
 	private static String CURRENT_LOSING_STREAK = "current_losing_streak";
-	private static String NUMBER_OF_TIES = "number_of_ties";
 	private static String TOTAL_GAMES = "total_games";
-	private static String HIGH_SCORE = "high_score";
 
 	/**
 	 * Constructor Takes and keeps a reference of the passed context in order to
@@ -54,8 +52,7 @@ public class DatabaseAdapter extends SQLiteOpenHelper {
 		String createUserTable = String
 				.format("CREATE TABLE IF NOT EXISTS %s (%s INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, %s TEXT NOT NULL, %s TEXT NOT NULL, %s TEXT NOT NULL, %s TEXT, %s TEXT, %s INTEGER, %s INTEGER, %s INTEGER, %s INTEGER, %s INTEGER, %s INTEGER, %s INTEGER)",
 						DATABASE_TABLE_USER, USER_ID, USERNAME, PASSWORD, EMAIL, FIRST_NAME, LAST_NAME, NUMBER_OF_WINS,
-						NUMBER_OF_LOSSES, HIGHEST_WINNING_STREAK, HIGHEST_LOSING_STREAK, CURRENT_WINNING_STREAK, CURRENT_LOSING_STREAK, NUMBER_OF_TIES, TOTAL_GAMES,
-						HIGH_SCORE);
+						NUMBER_OF_LOSSES, HIGHEST_WINNING_STREAK, HIGHEST_LOSING_STREAK, CURRENT_WINNING_STREAK, CURRENT_LOSING_STREAK, TOTAL_GAMES);
 		Log.i(TAG, String.format("%s", createUserTable));
 		db.execSQL(createUserTable);
 	}
