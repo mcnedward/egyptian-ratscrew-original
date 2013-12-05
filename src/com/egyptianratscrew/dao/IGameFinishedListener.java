ce938ab91f94853758057724edb2ab43cf9a068f
StopNowThink <StopNowThink@localhost>
2013-12-05 15:26:19 -0500

    Correcting gameover logic

--- src/com/egyptianratscrew/dao/IGameFinishedListener.java
+++ src/com/egyptianratscrew/dao/IGameFinishedListener.java
@@ -1,9 +1,9 @@
 package com.egyptianratscrew.dao;
 
-import com.egyptianratscrew.service.Game;
+import com.egyptianratscrew.service.Game2;
 
 public interface IGameFinishedListener {
 
-    public void onGameFinished(Game game);
+    public void onGameFinished(Game2 game);
 
 }
