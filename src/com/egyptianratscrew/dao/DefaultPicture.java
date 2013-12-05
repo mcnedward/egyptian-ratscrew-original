067e346c5e026d9b5515f3a95fb25865e267da53
StopNowThink <StopNowThink@localhost>
2013-12-05 13:44:15 -0500

    Fixed Card class and cardback select activity to use DefaultPicture class

--- src/com/egyptianratscrew/dao/DefaultPicture.java
+++ src/com/egyptianratscrew/dao/DefaultPicture.java
@@ -1,5 +1,13 @@
 package com.egyptianratscrew.dao;
 
-public class DefaultPicture {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
+public class DefaultPicture {
+	public static Bitmap getImage(Context context) {
+		int resourceId = context.getResources().getIdentifier("b2fv", "drawable", context.getPackageName());
+		Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), resourceId);
+		return bitmap;
+	}
 }
